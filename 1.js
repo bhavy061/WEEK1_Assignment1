ba4595cce473a8903a1205a9b56fc4a4fe9e7a6c
Your Full Name <bhavyachopra061@gmail.com>
2025-12-17 00:13:45 +0530

    Initial commit

---
+++ 1.js
@@ -0,0 +1,29 @@
+let txt = "Where are you"
+txt += " going"
+console.log(txt)
+
+let x = 5
+let y = "5" + 5
+let z = "Hello " + 5
+console.log(x)
+console.log(y)
+console.log(z)
+
+let p = 5
+let q = p ** 2
+console.log(q)
+
+function age()
+{
+    let a = 12
+    if(a < 10)
+    {
+        console.log("age is less than 12")
+    }
+    else
+    {
+        console.log("age is greater than 12")
+    }
+
+}
+age()
